0d76bdb2a1f98d902075dedd83257e3e757b8fdb
François Ferland <git@francoisferland.com>
2014-04-28 11:19:23 +0200

    Allow FindM3rt.cmake to find m3core files in the source dirs instead of /usr/local on first build.

--- Modules/FindM3rt.cmake
+++ Modules/FindM3rt.cmake
@@ -3,13 +3,18 @@
 set(FIND_M3RT_LIB_SUFFIX "")
 
 # find the M3RT include directory
+message ("!!!M3RT_SRC: ${M3RT_SRC}")
 find_path(M3RT_ROOT m3rt
 	PATH_SUFFIXES include
 		PATHS
 		/usr/local/
-		/usr)
+		/usr
+        ${M3RT_SRC})
 
-set(M3RT_ROOT ${M3RT_ROOT}/m3rt/)
+
+#set(M3RT_ROOT ${M3RT_ROOT}/m3rt/)
+
+message("!!!M3RT ROOT IS ${M3RT_ROOT}")
 
 # find the requested modules
 set(M3RT_FOUND TRUE) # will be set to false if one of the required modules is not found
@@ -21,7 +26,7 @@ set(FIND_M3RT_COMPONENT "base")
 string(TOLOWER ${FIND_M3RT_COMPONENT} FIND_M3RT_COMPONENT_LOWER)
 set(FIND_M3RT_COMPONENT_NAME m3${FIND_M3RT_COMPONENT_LOWER}${FIND_M3RT_LIB_SUFFIX})
 
-set(M3RT_INCLUDE_DIR ${M3RT_INCLUDE_DIR} ${M3RT_ROOT}${FIND_M3RT_COMPONENT_LOWER})
+set(M3RT_INCLUDE_DIR ${M3RT_INCLUDE_DIR} ${M3RT_ROOT} ${M3RT_ROOT}/m3rt/${FIND_M3RT_COMPONENT_LOWER})
 find_library(M3RT_${FIND_M3RT_COMPONENT} NAMES ${FIND_M3RT_COMPONENT_NAME} PATHS ${FIND_M3RT_LIB_PATHS} PATH_SUFFIXES lib)
 
 set(M3RT_LIBRARIES ${M3RT_LIBRARIES} ${M3RT_${FIND_M3RT_COMPONENT}})
